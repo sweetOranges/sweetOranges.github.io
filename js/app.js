274abd6f235f32e26b377d9e4b5828fd3b9e6267
swwetorange <tianchengemail@gmail.com>
2020-09-28 16:40:12 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,45 +1,24 @@
+
 var IDE = (function(){
 	var g_editer;
 	var g_worker;
 	var g_id = 0;
 	var g_task = {};
+	var g_data = [];
 
 	var data_url = 'https://gw.alipayobjects.com/os/antvdemo/assets/data/nintendo.json';
 	var worker_url = './js/worker.js';
-	var gist_url = 'https://api.github.com/users/sweetOranges/gists';
 
 	ace.config.set('basePath', 'https://cdn.bootcdn.net/ajax/libs/ace/1.4.9/');
 
 
-	function sendWorker(req) {
+	function send2Worker(req) {
 		return new Promise(function(resolve, reject) {
 			var task_id = g_id++;
 			req.id = task_id;
 			g_worker.postMessage(req);
 			g_task[task_id] = {resolve: resolve, reject: reject};
 		});
-	}
-
-
-	function init_gists() {
-		log('info', 'init_gists start');
-		var p = sendWorker({
-			method: 'request',
-			data: {
-				url: gist_url
-			}
-		})
-		p.then((res) => {
-			var data = JSON.parse(res);
-			var html = data.map(function(item) {
-				return Object.keys(item.files).map(function(file) {
-					return `<li>${item.files[file].filename}</li>`;
-				}).join('');
-			}).join('');
-			$('#left-menu ul').html(html);
-			log('info', 'gitsts init success');
-		});
-		
 	}
 
 	function init_worker() {
@@ -70,18 +49,20 @@ var IDE = (function(){
 	    log('info', 'init_editer success');
 	}
 
-	function render_table(data) {
-		var keys = Object.keys(data[0]);
+	function render_table() {
+		$('#dv-editor').hide();
+		$('#table').show();
+		var keys = Object.keys(g_data[0]);
         var thead = keys.map(function(key) {
                 return `<th style=" position: sticky;top: 0;background:#fff;box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4);">${key}</th>`
         }).join('');
-        var tbody = data.map(function(item){
+        var tbody = g_data.map(function(item){
                 return '<tr>' + keys.map(function(key){
                         return `<td>${item[key]}</td>`
                 }).join('') + "</tr>";
         });
         var html = `<thead>${thead}</thead><tbody>${tbody}</tbody>`;
-        $('#table').html(html);
+        $('#table table').html(html);
 	}
 
 	function log(l, msg) {
@@ -89,12 +70,34 @@ var IDE = (function(){
 		$("#console").scrollTop = $("#console").scrollHeight;
 	}
 
+	function load_data() {
+		send2Worker({
+			method: 'request',
+			data: {
+				url: data_url
+			}
+		}).then(function(res) {
+			var data = JSON.parse(res);
+			g_data = data;
+			log('info', 'load_data success');
+		});
+	}
+
+
+	function render_code() {
+		$('#dv-editor').show();
+		$('#table').hide();
+	}
+
 	function init() {
 		init_editer();
 		init_worker();
-		init_gists();
+		//init_gists();
 	}
 	return {
-		init: init
+		init: init,
+		load_data: load_data,
+		render_table: render_table,
+		render_code: render_code
 	}
 })();
